c619f4f6836a6f52dc03cb6210b98381839ef50c
root <root@vultr.guest>
2025-06-25 12:20:52 +0000

    add init mes model

---
+++ main/include/mes.hpp
@@ -0,0 +1,64 @@
+#ifndef MES_HPP
+#define MES_HPP
+
+#include <iostream>
+#include "cadmium/modeling/devs/atomic.hpp"
+
+using namespace cadmium;
+
+struct MESState {
+	bool idle;
+	bool initiatingOrder;
+
+    explicit MESState(): idle(true), initiatingOrder(false) {}
+};
+
+// Configures log format.
+std::ostream& operator<<(std::ostream &out, const MESState& state) {
+    out  << "{ idle: " << state.idle << ", initiatingOrder: " << state.initiatingOrder << " }";
+    return out;
+}
+
+// Atomic DEVS model of a Manufacturing Execution System (MES).
+class MES : public Atomic<MESState> {
+public:
+	Port<std::string> orderPlaced;
+	Port<std::string> orderInitiated;
+
+    // Constructor.
+    MES(const std::string id) : Atomic<MESState>(id, MESState()) {
+		orderPlaced = addInPort<std::string>("orderPlaced");
+		orderInitiated = addOutPort<std::string>("orderInitiated");
+    }
+
+    void internalTransition(MESState& state) const override {
+		if (state.initiatingOrder) {
+			state.initiatingOrder = false;
+			state.idle = true;
+		}
+    }
+
+    void externalTransition(MESState& state, double e) const override {
+		if (state.idle && !orderPlaced->empty()) {
+			state.idle = false;
+			state.initiatingOrder = true;
+		}
+	}
+    
+    void output(const MESState& state) const override {
+        orderInitiated->addMessage("Order Initiated");
+    }
+
+    [[nodiscard]] double timeAdvance(const MESState& state) const override {     
+		if (state.idle) {
+        	return 2147483647; 	// Maximum value for an integer, i.e., infinity.
+				   				// Actually, since it's a double,
+				   				// the maximum value is ~= 1.797 x 10^308,
+				   				// but who's counting?
+		} else {
+			return 0;
+		}
+    }
+};
+
+#endif
--- main/include/top.hpp
+++ main/include/top.hpp
@@ -3,6 +3,7 @@
 
 #include "cadmium/modeling/devs/coupled.hpp"
 #include "customer.hpp"
+#include "mes.hpp"
 
 using namespace cadmium;
 
@@ -10,6 +11,10 @@ struct TopCoupled : public Coupled {
 
     TopCoupled(const std::string& id, std::vector<int> orders) : Coupled(id) {
         auto customer = addComponent<Customer>("customer", orders);
+	auto mes = addComponent<MES>("mes");
+
+	// Couple output ports to input ports
+	addCoupling(customer->orderPlaced, mes->orderPlaced);
     }
 };
 
